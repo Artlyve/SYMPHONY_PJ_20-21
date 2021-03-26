bdc2c64efdffb9b4150c6d37e231bd9b67157ace
HizeOok <47179772+HizeOok@users.noreply.github.com>
2021-03-26 12:19:55 +0100

    fg
    gf

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class SecurityController extends AbstractController
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @Route("/inscription", name="security_registration")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function registration(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+
+        $em = $this->em;
+        $user = new Users();
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout(){
+    }
+}
